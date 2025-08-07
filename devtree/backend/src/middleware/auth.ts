bebbc8d557e326faff7d04ee38efee90be98de06
Adangabriel Tarazona <adangabriel.tarazona@bbva.com>
2025-08-07 11:10:09 -0300

    actulizando front editar_usuario && validaciones del back

---
+++ devtree/backend/src/middleware/auth.ts
@@ -0,0 +1,46 @@
+import type {Request, Response, NextFunction} from "express";
+import jwt from "jsonwebtoken";
+import User, {IUser} from "../models/User";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: IUser;
+        }
+    }
+}
+
+
+export const authenticate = async (req : Request, res: Response, next: NextFunction) => {
+    const bearer = req.headers.authorization;
+
+    if(!bearer) {
+        const error = new Error('No autorizado');
+        res.status(401).json({ error: error.message });
+        return;
+    }
+
+    const [, token] = bearer.split(' ');
+    if(!token) {
+        const error = new Error('No autorizado');
+        res.status(401).json({ error: error.message });
+        return;
+    }
+
+    try {
+        const result = jwt.verify(token, process.env.JWT_SECRET as string);
+        if(typeof result === 'object' && result.id){
+            const user = await User.findById(result.id).select('-password');
+            if(!user) {
+                const error = new Error('Usuario no encontrado');
+                res.status(404).json({ error: error.message });
+                return;
+            }
+            req.user = user;
+            next();
+        }
+    } catch (error) {
+       res.status(500).json({ error: 'Token no valido' });
+    }
+
+}
---
+++ devtree/frontend/src/api/DevTreeAPI.ts
@@ -0,0 +1,27 @@
+import { isAxiosError } from "axios";
+import api from "../config/axios";
+import { ProfileForm, User } from "../types";
+
+export async function getUser() {
+    try {
+        const { data } = await api<User>('user');
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+
+    }
+}
+
+export async function updateProfile(formData : ProfileForm) {
+    try {
+        const { data } = await api.patch<string>('/user', formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+
+    }
+}
